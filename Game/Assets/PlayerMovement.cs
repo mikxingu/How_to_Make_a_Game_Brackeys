bc7b796e8c82b9c9144f3b389259fe9ab509763f
Mike Home <michelalvs@yahoo.com.br>
2020-04-13 16:54:43 -0300

    EP03 - Implemented Player Controlls

--- Game/Assets/PlayerMovement.cs
+++ Game/Assets/PlayerMovement.cs
@@ -6,10 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
 	public Rigidbody rb;
 
+	public float forwardForce = 2000f;
+
+	public float sideForce = 500f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0,0,2000 * Time.deltaTime);
-    }
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+		if (Input.GetKey(KeyCode.D))
+		{
+			rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+		}
+	}
 }
