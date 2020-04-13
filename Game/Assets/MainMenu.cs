83e48ba2c6d27c472a260c9090da885fed4ec73d
Mike Home <michelalvs@yahoo.com.br>
2020-04-13 19:53:08 -0300

    Finished the tutorials and made build 1.0

---
+++ Game/Assets/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void StartGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+}
