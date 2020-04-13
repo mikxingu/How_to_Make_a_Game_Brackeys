742fe649f4161abf7d5e5a5df873e2475c2024ad
Mike Home <michelalvs@yahoo.com.br>
2020-04-13 17:23:53 -0300

    EP 06 - Improved player controls, implemented fog and obstacle prefab

--- Game/Assets/PlayerMovement.cs
+++ Game/Assets/PlayerMovement.cs
@@ -16,12 +16,12 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(0,0,forwardForce * Time.deltaTime);
 		if (Input.GetKey(KeyCode.D))
 		{
-			rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+			rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
 		if (Input.GetKey(KeyCode.A))
 		{
-			rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+			rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 	}
 }
